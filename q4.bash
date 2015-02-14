0b47614d7fb93a1ca64634e214823630425d55e4
Sean Stephenson <sms@pixar.com>
2015-02-14 13:46:53 -0800

    refactor q4

--- q4.bash
+++ q4.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-curl -sO 'http://biostat.jhsph.edu/~jleek/contact.html'
-sed -n 10p contact.html | wc -m
-sed -n 20p contact.html | wc -m
-sed -n 30p contact.html | wc -m
-sed -n 100p contact.html | wc -m
-rm -f contact.html
+# How many characters are in the 10th, 20th, 30th and 100th lines of HTML from this page: 
+# http://biostat.jhsph.edu/~jleek/contact.html 
+
+# I know, I know ... this is an R class ... but it's just too tempting to solve this with 
+# a single line on shell scripting
+
+curl -s 'http://biostat.jhsph.edu/~jleek/contact.html' | sed -n -e 10p -e 20p -e 30p -e 100p | awk '{ print length($0); }'
